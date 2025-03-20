7274e838106cf584206e1a212b17c794ae0e2517
Landeryou05 <landeryou05@gmail.com>
2025-03-20 00:07:27 +0000

    Major changes, initialised UI

---
+++ src/main/java/UI/UI_MainFrame.java
@@ -0,0 +1,10 @@
+package UI;
+
+import javax.swing.*;
+
+public class UI_MainFrame {
+    public static void main(String[] args){
+        UI_MainMenu mainMenu = new UI_MainMenu();
+        mainMenu.MainMenu_Frame();
+    }
+}
