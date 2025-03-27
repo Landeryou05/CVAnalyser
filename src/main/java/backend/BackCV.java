96dbc0755c192a4cfa8803d291c2ea141b1f427b
Landeryou05 <landeryou05@gmail.com>
2025-03-27 21:12:11 +0000

    CoreNLP implemented with dummy text

--- src/main/java/backend/BackCV.java
+++ src/main/java/backend/BackCV.java
@@ -6,52 +6,108 @@ import java.util.Properties;
 import edu.stanford.nlp.pipeline.*;
 
 public class BackCV {
+    // Instance Variables
     private String cvFileType = null;
     private String cvFile = null;
     private Boolean cvSubmit = false;
+    private String cvText = CVText();
 
-    public void CVTextAnalyser(){
-        String text = "github data scientist Microsoft skilled in Python, machine learning, and NLP. "
-                + "Worked at XYZ Corp developing software models and programming improving data pipelines."
-                + "Proficient in TensorFlow, Matt Landeryou, and SQL.";
+    ArrayList<String> extractedNameArrayList = new ArrayList<>();
+    ArrayList<String> extractedOrganisationsArrayList = new ArrayList<>();
 
-        ArrayList<String> cvKeywords = new ArrayList<>();
-        cvKeywords.add("python");
-        cvKeywords.add("Java");
-        cvKeywords.add("Programming");
-        cvKeywords.add("Software");
-        cvKeywords.add("github");
+    ArrayList<String> cvKeywordsArrayList = new ArrayList<>();
+    ArrayList<String> extractedKeywordsArrayList = new ArrayList<>();
 
-        // Stanford NLP
+    // Getters and Setters
+    public ArrayList<String> getExtractedName(){
+        return extractedNameArrayList;
+    }
+
+    public void setExtractedName(String extractedName){
+        extractedNameArrayList.add(extractedName);
+    }
+
+    public ArrayList<String> getExtractedOrganisations(){
+        return extractedOrganisationsArrayList;
+    }
+
+    public void setExtractedOrganisations(String extractedOrganisation){
+        extractedOrganisationsArrayList.add(extractedOrganisation);
+    }
+
+    public ArrayList<String> getCVKeywords(){
+        return cvKeywordsArrayList;
+    }
+
+    public void setCVKeywords(String keywords){
+        cvKeywordsArrayList.add(keywords);
+    }
+
+    public ArrayList<String> getExtractedKeywords(){
+        return extractedKeywordsArrayList;
+    }
+
+    public void setExtractedKeywords(String extractedKeywords){
+        extractedKeywordsArrayList.add(extractedKeywords);
+    }
+
+    // Methods
+    public void CVAnalyserMain(){
+        CVTextAnalyserNLP();
+        CVTextAnalyserKeywords();
+
+        System.out.println(getExtractedName());
+        System.out.println(getExtractedOrganisations());
+        System.out.println(getExtractedKeywords());
+    }
+
+    public void CVTextAnalyserNLP(){
         Properties properties = new Properties();
         properties.setProperty("annotators", "tokenize,pos,lemma,ner");
         StanfordCoreNLP pipeLine = new StanfordCoreNLP(properties);
 
-        CoreDocument document = new CoreDocument(text);
+        CoreDocument document = new CoreDocument(cvText);
         pipeLine.annotate(document);
-
-        ArrayList<String> extractedName = new ArrayList<>();
-        ArrayList<String> extractedOrganisations = new ArrayList<>();
-        ArrayList<String> extractedKeyWords = new ArrayList<>();
 
         ArrayList<String> falsePositives = new ArrayList<>();
         falsePositives.add("Python");
+        falsePositives.add("Github");
 
         for(CoreEntityMention nameEntity : document.entityMentions()){
             if(nameEntity.entityType().equals("PERSON") && !falsePositives.contains(nameEntity.text())){
-                extractedName.add(nameEntity.text());
+                setExtractedName(nameEntity.text());
             }
         }
 
         for(CoreEntityMention organisation : document.entityMentions()) {
             if (organisation.entityType().equals("ORGANIZATION")) {
-                extractedOrganisations.add(organisation.text());
+                setExtractedOrganisations(organisation.text());
             }
         }
+    }
 
-        System.out.println(extractedName);
-        System.out.println(extractedOrganisations);
-        System.out.println(extractedKeyWords);
+    public void CVTextAnalyserKeywords(){
+        setCVKeywords("python");
+        setCVKeywords("java");
+        setCVKeywords("programming");
+        setCVKeywords("software");
+        setCVKeywords("github");
+
+        cvText = cvText.toLowerCase();
+
+        for(int i = 0; i < getCVKeywords().size(); i++){
+            if(cvText.contains(getCVKeywords().get(i))){
+                setExtractedKeywords(getCVKeywords().get(i));
+            }
+        }
+    }
+
+    public String CVText(){
+        String cvRawText = "github data scientist Microsoft skilled in Python, machine learning, and NLP. "
+                + "Worked at XYZ Corp developing Java software models and programming improving data pipelines."
+                + "Proficient in TensorFlow, Matt Landeryou, and SQL.";
+
+        return cvRawText;
     }
 }
 
--- src/main/java/backend/BackCVRanking.java
+++ src/main/java/backend/BackCVRanking.java
@@ -3,6 +3,6 @@ package backend;
 public class BackCVRanking {
     public static void main(String[] args){
         BackCV CV = new BackCV();
-        CV.CVTextAnalyser();
+        CV.CVAnalyserMain();
     }
 }
