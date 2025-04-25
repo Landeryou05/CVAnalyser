f9facaa9d668923628d646dc11f42a70d70f015b
Landeryou05 <landeryou05@gmail.com>
2025-04-25 21:36:36 +0100

    Added requirements for assignment brief

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,17 @@
+/*
+* SID
+* 2407888
+*
+* GROUP NAME
+* JAMH
+*
+* DEVIATIONS FROM PLAN
+* As part of the plan, there wasn't a GUI. I have since added a GUI and therefore, added more code that has wasn't initially planned (mainly within the 'frontend' package).
+* This has also had a direct impact on the usage of my planned classes (within the 'backend' package), as it deviates from how they are accessed and utilised.
+* I have also taken out the planned KeyboardReader class, as there is a GUI and console input doesn't need to be read.
+* There has also been some deviations in method, some class and variable names as I want to ensure clarity, simplicity and modularity throughout the entire program.
+* */
+
 import frontend.FrontMainFrame;
 
 /**
